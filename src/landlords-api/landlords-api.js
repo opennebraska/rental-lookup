36a3a14defba94ead982023c63b8bee457ea0d1d
dodgees <dodgees2@gmail.com>
2021-03-22 14:03:29 -0500

    Remove typescript file

--- src/landlords-api/landlords-api.js
+++ src/landlords-api/landlords-api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 import getLandlordApiUrl from "../Config";
 
-export const searchForProperties = async (search: string) => {
+export const searchForProperties = async (search) => {
     const landlordApiUrl  = getLandlordApiUrl();
     const {data: properties} = await axios.get(`${landlordApiUrl}/properties?search=${search}`);
     return properties;
 }
 
-export const getProperty = async (pin: string) => {
+export const getProperty = async (pin) => {
     const landlordApiUrl = getLandlordApiUrl();
     const { data: property } = await axios.get(`${landlordApiUrl}/properties/${pin}`);
     return property
